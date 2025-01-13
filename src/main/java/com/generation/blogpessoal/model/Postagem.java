123e4edbc7315c7bb4ff6221ffc48190a927c1bd
caroline <carolineribeiro1996@gmail.com>
2025-01-13 14:50:49 -0300

    Criar novo Projeto

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,61 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity                               //Tratar essa classe como uma entidade(uma tabela)
+@Table(name =  "tb_postagens")       //indica o nome da tabela no Banco de Dados
+public class Postagem {
+
+	@Id //Ele é a chave primária (Primary Key)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Auto incremento  
+	private Long id; 
+	
+	@NotBlank(message = "Esse Campo é Obrigatório!!") //Not Null
+	@Size (min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "Esse Campo é Obrigatório!!") //Not Null
+	@Size (min = 10, max = 100, message = "O atributo texto deve conter no mínimo 10 e no máximo 100 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp 
+	private LocalDateTime data;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+ 	} 
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+}
